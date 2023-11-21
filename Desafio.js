67a2753e04a02513aa6669cebd3b136bd70213e5
Daniel Caldas <89937551+DaniCaldas@users.noreply.github.com>
2023-11-21 17:18:57 -0300

    Update Desafio.js

--- Desafio.js
+++ Desafio.js
@@ -1,11 +1,16 @@
 const array = [];
 
 function numero(n){
-  for(var i=0; i < n; i++){
-    if(i % 3 == 0 || i % 5 == 0){
-      array.push(i);
+    if(n <= 0){
+       console.log("digite um número positivo")
     }
-  }
+    else{
+      for(var i=0; i < n; i++){
+        if(i % 3 == 0 || i % 5 == 0){
+          array.push(i);
+        }
+      }
+    }
 }
-numero(11); // valor da função pode ser alterado para testes!
+numero(0); // valor da função pode ser alterado para testes!
 console.log(array.reduce((acc, num) => acc + num, 0));
